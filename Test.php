67972d9f096963aa82e7f209da044b1f626cc33c
Jan <Jan.Mannherz@online.de>
2016-04-25 17:31:37 +0200

    Testunit

---
+++ Test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jan
+ * Date: 25.04.2016
+ * Time: 17:02
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+
+    public function test()
+    {
+    }
+}
