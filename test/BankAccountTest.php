bf4aa20b832df900bc0326d838069cb186a206a2
Jan Mannherz <jan.mannherz@online.de>
2016-05-03 10:35:07 +0200

    Testunit23

---
+++ test/BankAccountTest.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'c:\Users\Jan\PhpstormProjects\FoodRangerWeb\Sites\BankAccount.php';
+class BankAccountTest extends PHPUnit_Framework_TestCase {
+    /** @var BankAccount */
+    protected $_account;
+    protected function setUp()
+    {
+        $this->_account = new BankAccount();
+    }
+    public function testBalanceIsZeroWhenOpeningAccount() {
+        $this->assertEquals(0, $this->_account->getBalance());
+    }
+    public function testDepositIncreasesBalance() {
+        $this->_account->deposit(100);
+        $this->assertEquals(100, $this->_account->getBalance());
+    }
+    public function testWithdrawDecreasesBalance() {
+        $this->_account->withdraw(100);
+        $this->assertEquals(-100, $this->_account->getBalance());
+    }
+    /**
+     * @expectedException Exception
+     */
+    public function testDepositNegativeAmountThrowsException() {
+        $this->_account->deposit(-200);
+    }
+}
